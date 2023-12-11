1969a0062e436b642446ea1bd60882aacae0aa50
Leo Shen <58051347+Soldier4893@users.noreply.github.com>
2023-12-11 00:49:57 -0600

    cleanup

--- compartment_model_sensitivity.py
+++ compartment_model_sensitivity.py
@@ -113,6 +113,7 @@ class CoupledCompartments:
         Pks = np.random.exponential(scale=1.0, size=(6, 3))
         Tks = np.zeros((6, 3))
         Aks = np.zeros((6, 3))
+        a = [0, 0, 0]
         while True:
             kinetic_rates1 = np.array(
                 (self.kI, self.kE*self.numCs[0], (self.kF-h)*self.numCs[0],
@@ -151,6 +152,125 @@ class CoupledCompartments:
                 self.update_comparts(which_action, 0)
             elif mu[1] == 2:
                 self.update_comparts(which_action, 1)
+        #     a[mu[1]]+=1
+        # print(a)
+
+    def update_comparts(self, which_action, i):
+        if which_action == 0: # add compartment 0-10
+            self.add_compart(np.int32(np.random.randint(0, 11)), i)
+        elif which_action == 1: # remove compartment
+            self.remove_compart(self.random_sample_index(i), i)
+        elif which_action == 2: # split compartment
+            # randomly select compartment and record a random amount
+            amount = 0
+            which_compart = np.random.choice(np.arange(self.numCs[i]))
+            if self.comparts[i][which_compart] != 0:
+                amount = np.int32(np.random.randint(0, self.comparts[i][which_compart]+1))
+            
+            # create a new compartment with the amount taken away
+            self.comparts[i][which_compart] -= amount
+            self.numSs[i] -= amount
+            self.add_compart(amount, i)
+        elif which_action == 3: # merge compartments
+            # randomly select compartment and record amount
+            which_compart = np.random.choice(np.arange(self.numCs[i]))
+            amount = self.comparts[i][which_compart]
+            
+            # add amount since remoce_compart automatically takes it away
+            self.numSs[i] += amount
+            self.remove_compart(which_compart, i)
+
+            # select new random compartment and increment
+            which_compart = np.random.choice(np.arange(self.numCs[i]))
+            self.comparts[i][which_compart] += amount
+        elif which_action == 4: # increment one compartment's chemicals
+            which_compart = np.random.choice(np.arange(self.numCs[i]))
+            self.comparts[i][which_compart] += 1
+            self.numSs[i] += 1
+        elif which_action == 5: # decrement one compartment's chemicals
+            comparts = self.comparts[i][:self.numCs[i]]
+            which_compart = np.random.choice(np.arange(self.numCs[i]), p = comparts/np.sum(comparts))
+            self.comparts[i][which_compart] -= 1
+            self.numSs[i] -= 1
+    
+    def getStats(self):
+        return self.numC, self.numR, self.numS, self.population[:self.numR]
+    
+    def add_compart(self, amount, i):
+        self.comparts[i][self.numCs[i]] = amount
+        self.numCs[i] += 1
+        self.numSs[i] += amount
+    
+    def remove_compart(self, index, i):
+        self.numSs[i] -= self.comparts[i][index]     
+        if index != self.numCs[i]-1:
+            self.comparts[i][index] = self.comparts[i][self.numCs[i]-1]            
+        self.numCs[i] -= 1
+
+    def random_sample_index(self, i):
+        return np.int32(np.random.randint(0, self.numCs[i]))
+    
+class CoupledCompartments2:
+    def __init__(self, ckappas, kB, kD):
+        self.comparts = np.zeros((2, 1024**2), dtype = np.int32)
+        self.kB = np.float64(kB)
+        self.kD = np.float64(kD)
+        self.kI = np.float64(ckappas[0])
+        self.kE = np.float64(ckappas[1])
+        self.kF = np.float64(ckappas[2])
+        self.kC = np.float64(ckappas[3])
+
+    def simComparts(self, time_limit, h):
+        self.comparts[0][0] = 1
+        self.comparts[1][0] = 1
+        self.numCs = [1, 1]
+        self.numSs = [1, 1]
+        timer = 0
+
+        Pks = np.random.exponential(scale=1.0, size=(6, 3))
+        Tks = np.zeros((6, 3))
+        Aks = np.zeros((6, 3))
+        a = [0, 0, 0]
+        while True:
+            kinetic_rates1 = np.array(
+                (self.kI, self.kE*self.numCs[0], (self.kF-h)*self.numCs[0],
+                  self.kC*self.numCs[0]*(self.numCs[0]-1)/2,
+                    self.kB*self.numCs[0], self.kD*self.numSs[0])
+            )
+            kinetic_rates2 = np.array(
+                (self.kI, self.kE*self.numCs[1], (self.kF+h)*self.numCs[1],
+                  self.kC*self.numCs[1]*(self.numCs[1]-1)/2,
+                    self.kB*self.numCs[1], self.kD*self.numSs[1])
+            )
+
+            for i in range(6):
+                Aks[i][0] = min(kinetic_rates1[i], kinetic_rates2[i])
+                Aks[i][1] = kinetic_rates1[i] - Aks[i][0]
+                Aks[i][2] = kinetic_rates2[i] - Aks[i][0]
+            
+            
+            with np.errstate(divide='ignore', invalid='ignore'):
+                delta_tks = np.where(Aks != 0, np.divide(Pks - Tks,Aks), np.inf)
+            
+            mu = np.unravel_index(np.nanargmin(delta_tks, axis=None), Aks.shape)
+            Tks += Aks * delta_tks[mu]
+            Pks[mu] += np.random.exponential(1)
+            timer += delta_tks[mu]
+
+            if timer >= time_limit:
+                break
+            
+            which_action = mu[0]
+
+            if mu[1] == 0:
+                self.update_comparts(which_action, 0)
+                self.update_comparts(which_action, 1)
+            elif mu[1] == 1:
+                self.update_comparts(which_action, 0)
+            elif mu[1] == 2:
+                self.update_comparts(which_action, 1)
+        #     a[mu[1]]+=1
+        # print(a)
 
     def update_comparts(self, which_action, i):
         if which_action == 0: # add compartment 0-10
--- simplefast_sensitivity.py
+++ simplefast_sensitivity.py
@@ -30,73 +30,75 @@ def compute_variance_over_time(x1, x2):
                 count2 += 1
     return data.T
 
-def plot_variances(timelength):
+def plot_variances(time_limit):
     comp = SimpleFastCompartmentManager((1, 1, 1.5 - h, 1), 1, 1)
-    comp.simComparts(timelength)
+    comp.simComparts(time_limit)
     C,R,S,P1 = comp.getStats()
 
     comp = SimpleFastCompartmentManager((1, 1, 1.5 + h, 1), 1, 1)
-    comp.simComparts(timelength)
+    comp.simComparts(time_limit)
     C,R,S,P2 = comp.getStats()
     x = compute_variance_over_time(P1, P2)
     plt.plot(x[0], x[1])
     plt.show()
 
-
-
-def mc_vs_crn(h, timelength, num_samples, num_iterations):
+def mc(h, time_limit, num_iterations):
     samplesl1 = np.zeros(num_iterations)
     samplesr1 = np.zeros(num_iterations)
-    samplesl2 = np.zeros(num_iterations)
-    samplesr2 = np.zeros(num_iterations)
 
     for i in range(num_iterations):
-        np.random.seed(np.random.randint(0, 100))
+        np.random.seed(np.random.randint(0, num_iterations * 10))
 
         comp = SimpleFastCompartmentManager((1, 1, 1.5 - h, 1), 1, 1)
-        comp.simComparts(timelength)
+        comp.simComparts(time_limit)
         samplesl1[i]=comp.numC
         del comp
 
         comp = SimpleFastCompartmentManager((1, 1, 1.5 + h, 1), 1, 1)
-        comp.simComparts(timelength)
+        comp.simComparts(time_limit)
         samplesr1[i]=comp.numC
         del comp
+    return (samplesr1-samplesl1)/(2*h*num_iterations)
+
+def crn(h, time_limit, num_iterations):
+    samplesl2 = np.zeros(num_iterations)
+    samplesr2 = np.zeros(num_iterations)
 
     np.random.seed()
     for i in range(num_iterations):
         comp = SimpleFastCompartmentManager((1, 1, 1.5 - h, 1), 1, 1)
-        comp.simComparts(timelength)
+        comp.simComparts(time_limit)
         samplesl2[i]=comp.numC
         del comp
 
         comp = SimpleFastCompartmentManager((1, 1, 1.5 + h, 1), 1, 1)
-        comp.simComparts(timelength)
+        comp.simComparts(time_limit)
         samplesr2[i]=comp.numC
         del comp
+    return  (samplesr2-samplesl2)/(2*h*num_iterations)
 
-    crn = (samplesr1-samplesl1)/(2*h*num_iterations)
-    nmc = (samplesr2-samplesl2)/(2*h*num_iterations)
-    return np.mean(crn), np.std(crn), np.mean(nmc), np.std(nmc)
-
-def cfd(num_iterations, step_size):
+def cfd(h, time_limit, num_iterations):
     samplesl1 = np.zeros(num_iterations)
     samplesr1 = np.zeros(num_iterations)
     for i in range(num_iterations):
         coupledCompart = CoupledCompartments((1, 1, 1.5, 1), 1, 1)
-        coupledCompart.simComparts(time_limit=5, h=step_size)
+        coupledCompart.simComparts(time_limit=time_limit, h=h)
         samplesl1[i] = coupledCompart.numCs[0]
         samplesr1[i] = coupledCompart.numCs[1]
-    return (samplesr1[i]-samplesl1[i])/(2*step_size*num_iterations)
+    return (samplesr1-samplesl1)/(2*h*num_iterations)
 
 if __name__ == "__main__":
-    # print(mc_vs_crn(h = 0.01, timelength = 5, num_samples = 60, num_iterations=100))
+    num_iterations = 1000
+    h=0.01
+    time_limit = 5
+    y = mc(h = h, time_limit = time_limit, num_iterations=num_iterations)
+    print('monte carlo:',np.mean(y), ' pm ',np.std(y),'\n')
 
-    # coupledCompart = CoupledCompartments((1, 1, 1.5, 1), 1, 1)
-    # coupledCompart.simComparts(time_limit=500, h=0.01)
-    # print(coupledCompart.numCs[0], coupledCompart.numCs[1])
-    x = cfd(1000, 1)
-    print(np.mean(x), ' pm ',np.std(x))
+    z = mc(h = h, time_limit = time_limit, num_iterations=num_iterations)
+    print('common rand:',np.mean(z), ' pm ',np.std(z),'\n')
+
+    x = cfd(h = 0.01, time_limit = time_limit, num_iterations=num_iterations)
+    print('coupled:',np.mean(x), ' pm ',np.std(x),'\n')
 
 
 
